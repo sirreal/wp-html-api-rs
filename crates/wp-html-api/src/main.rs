a72494f4c105c45373a3379bd761aeba36518d73
Jon Surrell <sirreal@users.noreply.github.com>
2025-02-14 21:52:04 +0100

    Remove output from bin

--- crates/wp-html-api/src/main.rs
+++ crates/wp-html-api/src/main.rs
@@ -13,25 +13,25 @@ pub fn main() {
     let mut p = HtmlProcessor::create_full_parser(html.as_bytes(), "UTF-8").unwrap();
 
     while p.next_token() {
-        match p.get_token_name() {
-            Some(node) => match node {
-                wp_html_api::tag_processor::NodeName::Tag(tag_name) => {
-                    let closer = if p.is_tag_closer() { "/" } else { "" };
-                    println!("{closer}{} // token number {}", tag_name, c);
-                }
-                wp_html_api::tag_processor::NodeName::Token(token_type) => {
-                    let s: String = token_type.into();
-                    println!("{}  // token number {}", s, c);
-                }
-            },
-            None => {}
-        };
+        //match p.get_token_name() {
+        //    Some(node) => match node {
+        //        wp_html_api::tag_processor::NodeName::Tag(tag_name) => {
+        //            let closer = if p.is_tag_closer() { "/" } else { "" };
+        //            println!("{closer}{} // token number {}", tag_name, c);
+        //        }
+        //        wp_html_api::tag_processor::NodeName::Token(token_type) => {
+        //            let s: String = token_type.into();
+        //            println!("{}  // token number {}", s, c);
+        //        }
+        //    },
+        //    None => {}
+        //};
 
         c += 1;
     }
-    if p.get_last_error().is_some() {
-        println!("{:?}", p.get_last_error());
-    }
+    //if p.get_last_error().is_some() {
+    //    println!("{:?}", p.get_last_error());
+    //}
 
     println!("Found {c} tokens!");
 }
