22a176ce7535472292a11f2e05c342e48454ac8c
Jon Surrell <sirreal@users.noreply.github.com>
2025-03-06 18:11:17 +0100

    Tweak demo and build-and-run

--- build-and-run.sh
+++ build-and-run.sh
@@ -5,4 +5,4 @@ set -u
 
 cargo build --release
 ./target/release/cargo-php stubs --manifest crates/wp-html-api-php-ext/Cargo.toml
-php -d extension=target/release/libwp_html_api_php_ext.dylib demo.php -v
+php -d extension=target/release/libwp_html_api_php_ext.dylib demo.php -i data/html-standard.html
--- demo.php
+++ demo.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+// Buffer output to reduce impact of printing on timings.
+ob_start();
+
 $opts = getopt('i:v', ['color::']);
 $use_color  = match ( $opts['color'] ?? null ) {
     'never'         => false,
@@ -25,40 +28,53 @@ if ( null === $input_file ) {
     $html = file_get_contents( $input_file );
 }
 
-$processor = WP_HTML_Processor::create_full_parser($html);
+$make_html_processor = function() use (&$html) {
+	return WP_HTML_Processor::create_full_parser($html);
+};
+$make_tag_processor = function() use (&$html) {
+	return new WP_HTML_Tag_Processor($html);
+};
 
-$c = 0;
-$ns = -hrtime( true );
-if ( $is_verbose ) {
-    while ($processor->next_token()) {
-        $c++;
+foreach ([
+	[ WP_HTML_Processor::class, $make_html_processor ],
+	[ WP_HTML_Tag_Processor::class, $make_tag_processor ],
+] as [ $class, $make_processor ]) {
+	ob_flush();
+	$processor = $make_processor();
+	$c = 0;
+	$ns = -hrtime( true );
+	if ( $is_verbose ) {
+		while ($processor->next_token()) {
+			$c++;
 
-        switch ( $processor->get_token_type() ) {
-            case '#tag':
-                echo $use_color
-                    ? "\e[2;35m<\e[0;34m{$processor->get_token_name()}\e[2;35m>\e[m\n"
-                    : "<{$processor->get_token_name()}>\n";
-                break;
+			switch ( $processor->get_token_type() ) {
+				case '#tag':
+					echo $use_color
+						? "\e[2;35m<\e[0;34m{$processor->get_token_name()}\e[2;35m>\e[m\n"
+						: "<{$processor->get_token_name()}>\n";
+					break;
 
-            case '#text':
-                echo $use_color ? "\e[2;90m#text\e[m\n" : "#text\n";
-                break;
-        }
-    }
-} else {
-    while ($processor->next_token()) {
-        $c++;
-    }
-}
-$ns  += hrtime( true );
-$n    = new NumberFormatter( 'en-US', NumberFormatter::DEFAULT_STYLE );
-$ms   = $n->format( $ns / 1e6 );
-$mbps = $n->format( strlen( $html ) / 1e6 / ( $ns / 1e9 ) );
+				case '#text':
+					echo $use_color ? "\e[2;90m#text\e[m\n" : "#text\n";
+					break;
+			}
+		}
+	} else {
+		while ($processor->next_token()) {
+			$c++;
+		}
+	}
+	$ns  += hrtime( true );
+	$n    = new NumberFormatter( 'en-US', NumberFormatter::DEFAULT_STYLE );
+	$ms   = $n->format( $ns / 1e6 );
+	$mbps = $n->format( strlen( $html ) / 1e6 / ( $ns / 1e9 ) );
 
-if ( $use_color ) {
-    echo "\e[90mTook \e[33m{$ms}\e[2mms\e[0;90m (\e[34m{$mbps}\e[2mMB/s\e[0;90m)\e[m\n";
-    echo "\e[90mFound \e[36m{$c}\e[90m tokens.\e[m\n";
-} else {
-    echo "Took {$ms}ms ({$mbps}MB/s)\n";
-    echo "Found {$c} tokens.\n";
+	echo "With {$class}\n";
+	if ( $use_color ) {
+		echo "\t\e[90mTook \e[33m{$ms}\e[2mms\e[0;90m (\e[34m{$mbps}\e[2mMB/s\e[0;90m)\e[m\n";
+		echo "\t\e[90mFound \e[36m{$c}\e[90m tokens.\e[m\n";
+	} else {
+		echo "\tTook {$ms}ms ({$mbps}MB/s)\n";
+		echo "\tFound {$c} tokens.\n";
+	}
 }
