726257e3c770b7d348c55d207479525830394c66
Jon Surrell <sirreal@users.noreply.github.com>
2025-03-13 17:39:19 +0100

    Use string at compile time for benches

--- crates/wp-html-api/benches/process-html-standard.rs
+++ crates/wp-html-api/benches/process-html-standard.rs
@@ -1,15 +1,17 @@
 use wp_html_api::html_processor::HtmlProcessor;
 use wp_html_api::tag_processor::TagProcessor;
 
+const INPUT: &[u8] = include_bytes!("../../../data/html-standard.html");
+
 fn main() {
     divan::main();
 }
 
 #[divan::bench(skip_ext_time = true)]
 fn bench_html_processor(bencher: divan::Bencher) {
-    bencher.with_inputs(get_input).bench_values(|input| {
+    bencher.bench(|| {
         let mut processor =
-            HtmlProcessor::create_full_parser(&input, "UTF-8").expect("Processor must read input");
+            HtmlProcessor::create_full_parser(INPUT, "UTF-8").expect("Processor must read input");
         while processor.next_token() {}
         processor
     });
@@ -17,13 +19,9 @@ fn bench_html_processor(bencher: divan::Bencher) {
 
 #[divan::bench(skip_ext_time = true)]
 fn bench_tag_processor(bencher: divan::Bencher) {
-    bencher.with_inputs(get_input).bench_values(|input| {
-        let mut processor = TagProcessor::new(&input);
+    bencher.bench(|| {
+        let mut processor = TagProcessor::new(INPUT);
         while processor.next_token() {}
         processor
     });
-}
-
-fn get_input() -> Vec<u8> {
-    std::fs::read("../../data/html-standard.html").expect("Missing input!")
 }
