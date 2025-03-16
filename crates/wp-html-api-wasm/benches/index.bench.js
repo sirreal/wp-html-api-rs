2ad72493ec2effe3d0b4e82ff6393ce7a99b9880
Jon Surrell <sirreal@users.noreply.github.com>
2025-03-16 19:09:15 +0100

    Add benchmarks for WASM build

---
+++ crates/wp-html-api-wasm/benches/index.bench.js
@@ -0,0 +1,28 @@
+import fs from "node:fs";
+const html = fs.readFileSync(
+	new URL("../../../data/html-standard.html", import.meta.url),
+	"utf8",
+);
+import { bench, describe } from "vitest";
+import { assert } from "node:console";
+import { WP_HTML_Processor, WP_HTML_Tag_Processor } from "../../../pkg-node/wp_html_api_wasm.js";
+
+bench("WP_HTML_Processor", () => {
+	const processor = WP_HTML_Processor.create_full_parser(html);
+
+	let c = 0;
+	while (processor.next_token()) {
+		c++;
+	}
+	assert(c === 1_040_654);
+});
+
+bench("WP_HTML_Tag_Processor", () => {
+	const processor = new WP_HTML_Tag_Processor(html);
+
+	let c = 0;
+	while (processor.next_token()) {
+		c++;
+	}
+	assert(c === 938_062);
+});
