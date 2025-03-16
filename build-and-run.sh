fe70ece4ce93b474ae51a9e110276ba0682e9070
Jon Surrell <sirreal@users.noreply.github.com>
2025-03-16 19:26:10 +0100

    Remove more submodule/crate usage

--- build-and-run.sh
+++ build-and-run.sh
@@ -3,8 +3,7 @@
 set -e
 set -u
 
-cargo build --release --quiet -p wp-html-api-php-ext -p cargo-php
-./target/release/cargo-php stubs --manifest crates/wp-html-api-php-ext/Cargo.toml
+cargo build --release --quiet -p wp-html-api-php-ext
 echo "Running demo.php"
 php -d extension=target/release/libwp_html_api_php_ext.dylib demo.php
 
