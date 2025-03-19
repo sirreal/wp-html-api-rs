315f25739a230a71100e5d8cb602ef5f20903b86
Jon Surrell <sirreal@users.noreply.github.com>
2025-03-19 15:58:15 +0100

    Use tinybench instead of vitest (#23)
    Much smaller and faster install. Vitest is using tinybench anyways.

--- crates/wp-html-api-wasm/benches/index.bench.js
+++ crates/wp-html-api-wasm/benches/index.bench.js
@@ -1,28 +1,58 @@
 import fs from "node:fs";
+import assert from "node:assert";
+import {
+	WP_HTML_Processor,
+	WP_HTML_Tag_Processor,
+} from "../../../pkg-node/wp_html_api_wasm.js";
+import { Bench } from "tinybench";
+import { withCodSpeed } from "@codspeed/tinybench-plugin";
 const html = fs.readFileSync(
 	new URL("../../../data/html-standard.html", import.meta.url),
 	"utf8",
 );
-import { bench, describe } from "vitest";
-import { assert } from "node:console";
-import { WP_HTML_Processor, WP_HTML_Tag_Processor } from "../../../pkg-node/wp_html_api_wasm.js";
+const bench = withCodSpeed(
+	new Bench({
+		throws: true,
+	}),
+);
 
-bench("WP_HTML_Processor", () => {
-	const processor = WP_HTML_Processor.create_full_parser(html);
+let htmlProcessor;
+bench.add(
+	"WP_HTML_Processor",
+	() => {
+		htmlProcessor = WP_HTML_Processor.create_full_parser(html);
 
-	let c = 0;
-	while (processor.next_token()) {
-		c++;
-	}
-	assert(c === 1_040_654);
-});
+		let c = 0;
+		while (htmlProcessor.next_token()) {
+			c++;
+		}
+		assert(c === 1_040_654);
+	},
+	{
+		afterEach() {
+			htmlProcessor.free();
+		},
+	},
+);
 
-bench("WP_HTML_Tag_Processor", () => {
-	const processor = new WP_HTML_Tag_Processor(html);
+let tagProcessor;
+bench.add(
+	"WP_HTML_Tag_Processor",
+	() => {
+		tagProcessor = new WP_HTML_Tag_Processor(html);
 
-	let c = 0;
-	while (processor.next_token()) {
-		c++;
-	}
-	assert(c === 938_062);
-});
+		let c = 0;
+		while (tagProcessor.next_token()) {
+			c++;
+		}
+		assert(c === 938_062);
+	},
+	{
+		afterEach() {
+			tagProcessor.free();
+		},
+	},
+);
+
+await bench.run();
+console.table(bench.table());
--- crates/wp-html-api-wasm/benches/vitest.config.ts
+++
@@ -1,6 +0,0 @@
-import codspeedPlugin from "@codspeed/vitest-plugin";
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-	plugins: [codspeedPlugin()],
-});
