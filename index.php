3080de0dd5297aaaf1dce0ecb8f47a536077cc50
Jon Surrell <sirreal@users.noreply.github.com>
2025-03-20 11:52:41 +0100

    Explain index.php file

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-require_once __DIR__ . '/wp-html-api-php-ext.stubs.php';
 
+/**
+ * This file exists to help IDEs and static analysis tools recognize the
+ * generated stubs for the PHP extension.
+ *
+ * @link https://crates.io/crates/cargo-php
+ */
+
+require_once __DIR__ . '/wp-html-api-php-ext.stubs.php';
